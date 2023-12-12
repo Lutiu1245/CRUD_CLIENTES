38ce6e8e242553a45a723471c9c0025abf2ce599
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-12-12 13:58:06 -0300

    feat: add entity client

---
+++ src/main/java/com/example/CRUD_CLIENTES/entities/Client.java
@@ -0,0 +1,31 @@
+package com.example.CRUD_CLIENTES.entities;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private double income;
+    private LocalDate birthDate;
+    private int children;
+
+    public Client(Long id, String name, String cpf, double income, LocalDate birthDate, int children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public Client() {
+
+    }
+}
