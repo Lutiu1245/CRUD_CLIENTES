d5075be919c931d19ac2dbf3449bf31cac9fc806
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-12-12 15:35:41 -0300

    feat: insert data to the tb_client table

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('João Silva', '123.456.789-00', 5000.00, '1990-01-15', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria Oliveira', '987.654.321-00', 8000.00, '1985-07-20', 1);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos Souza', '111.222.333-44', 6000.00, '1995-03-10', 0);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana Santos', '444.555.666-77', 7000.00, '1992-11-05', 3);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Rafaela Lima', '888.999.000-11', 5500.00, '1988-09-18', 2);
