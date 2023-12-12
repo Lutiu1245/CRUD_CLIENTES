9f261aed1c086a7b899906fced0132322b721e43
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-12-12 14:31:27 -0300

    feat: add validation for ClientDTO

--- src/main/java/com/example/CRUD_CLIENTES/DTO/ClientDTO.java
+++ src/main/java/com/example/CRUD_CLIENTES/DTO/ClientDTO.java
@@ -1,16 +1,37 @@
 package com.example.CRUD_CLIENTES.DTO;
 
+import com.example.CRUD_CLIENTES.entities.Client;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.br.CPF;
+
 import java.time.LocalDate;
 
 public class ClientDTO {
     private Long id;
+    @Size(min = 3, max = 14, message = "Nome deve ter entre 3 e 14 caracteres")
+    @NotBlank(message = "Campo de nome é obrigatório")
     private String name;
+    @CPF(message = "CPF inválido")
     private String cpf;
+    @Positive(message = "Renda deve ser uma valor positivo")
     private double income;
+    @NotBlank()
     private LocalDate birthDate;
+    @Positive(message = "Este campo deve ser positivo")
     private int children;
 
     public ClientDTO(Long id, String name, String cpf, double income, LocalDate birthDate, int children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public ClientDTO(Client client) {
         this.id = id;
         this.name = name;
         this.cpf = cpf;
