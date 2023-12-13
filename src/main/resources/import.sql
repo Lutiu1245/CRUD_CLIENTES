b0211e7b2baa4397677e00436eed6f60a69bc8aa
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-12-13 09:59:55 -0300

    feat: add more clients in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,3 +7,23 @@ INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos 
 INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana Santos', '444.555.666-77', 7000.00, '1992-11-05', 3);
 
 INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Rafaela Lima', '888.999.000-11', 5500.00, '1988-09-18', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Miguel Pereira', '543.210.987-65', 4800.00, '1996-07-12', 1);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Antônia Souza', '654.321.098-76', 7500.00, '1991-05-23', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucas Costa', '765.432.109-87', 5900.00, '1998-09-06', 0);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Sofia Silva', '876.543.210-98', 6800.00, '1993-01-14', 3);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Eduardo Oliveira', '987.654.321-10', 4300.00, '2000-03-27', 0);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Julia Santos', '098.765.432-11', 5200.00, '1997-11-09', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Gabriel Lima', '109.876.543-12', 4500.00, '2001-08-20', 1);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Isabella Pereira', '210.987.654-13', 3300.00, '2002-06-30', 0);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Felipe Souza', '321.098.765-14', 5500.00, '1989-12-25', 3);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Luísa Costa', '432.109.876-15', 7200.00, '1986-04-10', 2);
