3c1fd92c0d8816e40abaa7a235b45f5a5faab72d
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-12-13 08:47:00 -0300

    feat: CustomError and ControllerHandler

---
+++ src/main/java/com/example/CRUD_CLIENTES/DTO/CustomError.java
@@ -0,0 +1,33 @@
+package com.example.CRUD_CLIENTES.DTO;
+
+import java.time.Instant;
+
+public class CustomError {
+    private Instant instant;
+    private Integer status;
+    private String error;
+    private String path;
+
+    public CustomError(Instant instant, Integer status, String error, String path) {
+        this.instant = instant;
+        this.status = status;
+        this.error = error;
+        this.path = path;
+    }
+
+    public Instant getInstant() {
+        return instant;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
---
+++ src/main/java/com/example/CRUD_CLIENTES/controllers/handler/ControllerExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.example.CRUD_CLIENTES.controllers.handler;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+}
