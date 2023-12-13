2a2a95c6c33a5c86dac28cf06de2e57f9cab9e2a
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-12-13 09:59:36 -0300

    feat: add methodArgumentNotValidException

--- src/main/java/com/example/CRUD_CLIENTES/controllers/handler/ControllerExceptionHandler.java
+++ src/main/java/com/example/CRUD_CLIENTES/controllers/handler/ControllerExceptionHandler.java
@@ -1,12 +1,16 @@
 package com.example.CRUD_CLIENTES.controllers.handler;
 
 import com.example.CRUD_CLIENTES.DTO.CustomError;
+import com.example.CRUD_CLIENTES.DTO.FieldMessageDTO;
+import com.example.CRUD_CLIENTES.DTO.ValidationErrorDTO;
 import com.example.CRUD_CLIENTES.exceptions.DatabaseException;
 import com.example.CRUD_CLIENTES.exceptions.ResourceNotFoundExeception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -28,5 +32,16 @@ public class ControllerExceptionHandler {
         CustomError customError = new CustomError(Instant.now(), httpStatus.value(),
                 e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(httpStatus).body(customError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationErrorDTO validationErrorDTO = new ValidationErrorDTO(Instant.now(), httpStatus.value(),
+                "Dados invalidos", request.getRequestURI());
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            validationErrorDTO.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.status(httpStatus).body(validationErrorDTO);
     }
 }
