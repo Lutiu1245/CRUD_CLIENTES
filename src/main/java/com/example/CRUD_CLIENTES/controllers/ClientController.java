551fc5b671168ab1cd83309dda67ba0e14fab6a9
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2024-01-11 16:13:01 -0300

    refactor: rename ClientService for ClientServiceImpl

--- src/main/java/com/example/CRUD_CLIENTES/controllers/ClientController.java
+++ src/main/java/com/example/CRUD_CLIENTES/controllers/ClientController.java
@@ -1,7 +1,7 @@
 package com.example.CRUD_CLIENTES.controllers;
 
 import com.example.CRUD_CLIENTES.DTO.ClientDTO;
-import com.example.CRUD_CLIENTES.services.ClientService;
+import com.example.CRUD_CLIENTES.services.ClientServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,33 +16,33 @@ import java.net.URI;
 @RequestMapping(value = "/clients")
 public class ClientController {
     @Autowired
-    private ClientService clientService;
+    private ClientServiceImpl clientServiceImpl;
     @GetMapping(value = "{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
-        return ResponseEntity.ok(clientService.findById(id));
+        return ResponseEntity.ok(clientServiceImpl.findById(id));
     }
 
     @GetMapping()
     public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
-        return ResponseEntity.ok(clientService.findAll(pageable));
+        return ResponseEntity.ok(clientServiceImpl.findAll(pageable));
     }
 
     @PostMapping
     public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO clientDTO) {
-        clientDTO = clientService.insert(clientDTO);
+        clientDTO = clientServiceImpl.insert(clientDTO);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(clientDTO.getId()).toUri();
         return ResponseEntity.created(uri).body(clientDTO);
     }
 
     @PutMapping(value = "{id}")
     public ResponseEntity<ClientDTO> update(@PathVariable Long id,@Valid @RequestBody ClientDTO clientDTO){
-      clientDTO = clientService.update(id, clientDTO);
+      clientDTO = clientServiceImpl.update(id, clientDTO);
       return ResponseEntity.ok(clientDTO);
     }
 
     @DeleteMapping(value = "{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        clientService.delete(id);
+        clientServiceImpl.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
